b53c82555c3ea40cfd0106f8ea9d4b78f9953992
GusGD <si_gustavo@outlook.com>
2025-03-11 15:18:18 -0300

    Created UserDetailsProjection

--- src/main/java/com/gusgd/ecommerce/projections/UserDetailsProjection.java
+++ src/main/java/com/gusgd/ecommerce/projections/UserDetailsProjection.java
@@ -1,4 +1,8 @@
-package com.gusgd.ecommerce;
+package com.gusgd.ecommerce.projections;
 
 public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
 }
