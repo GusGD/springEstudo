3aa47c630dd68527fddd3a352494986a66e7ce2b
GusGD <si_gustavo@outlook.com>
2025-03-11 15:23:09 -0300

    UserService class created

--- src/main/java/com/gusgd/ecommerce/services/UserService.java
+++ src/main/java/com/gusgd/ecommerce/services/UserService.java
@@ -1,4 +1,34 @@
 package com.gusgd.ecommerce.services;
 
-public class UserService {
+import com.gusgd.ecommerce.entities.Role;
+import com.gusgd.ecommerce.entities.User;
+import com.gusgd.ecommerce.projections.UserDetailsProjection;
+import com.gusgd.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if(result.isEmpty()){
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.getFirst().getPassword());
+        for(UserDetailsProjection projection : result){
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+        return  user;
+    }
 }
