d51104a52178af96b23f00f8285387463bbf71a3
GusGD <si_gustavo@outlook.com>
2025-03-25 10:58:07 -0300

    created OrderItemDTO

---
+++ src/main/java/com/gusgd/ecommerce/dto/OrderItemDTO.java
@@ -0,0 +1,44 @@
+package com.gusgd.ecommerce.dto;
+
+import com.gusgd.ecommerce.entities.OrderItem;
+
+public class OrderItemDTO {
+    private Long productId;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public OrderItemDTO(OrderItem entity) {
+        productId = entity.getProduct().getId();
+        name = entity.getProduct().getName();
+        price = entity.getPrice();
+        quantity = entity.getQuantity();
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSubTotal(){
+        return (Double) (price * quantity);
+    }
+}
